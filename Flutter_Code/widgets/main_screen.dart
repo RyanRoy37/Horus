b8125729c0eaed78c5f86e38c6847e31158652ed
J Ryan Roy <104367213+RyanRoy37@users.noreply.github.com>
2025-07-02 20:05:18 +0530

    Create main_screen.dart

---
+++ Flutter_Code/widgets/main_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import '../screens/home_page.dart';
+import '../screens/photo_capture_page.dart';
+import '../screens/sound_capture_page.dart';
+import '../screens/bird_guide_page.dart';
+import '../screens/profile_page.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  
+  final List<Widget> _pages = [
+    HomePage(),
+    PhotoCapturePage(),
+    SoundCapturePage(),
+    BirdGuidePage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            label: 'Photo',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mic),
+            label: 'Sound',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Guide',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
